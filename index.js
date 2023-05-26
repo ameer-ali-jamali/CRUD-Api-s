a89808fd9dc379187f94926f1dcf93fda491d10d
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-27 02:54:43 +0500

    port testing done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi")
+const { response } = require('express')
+const http = require('http')
+const port = process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+    res.StatusCode = 200;
+    res.setHeader('Content-Type', 'text/html')
+    res.end("<h1>hello world</h1>")
+})
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
