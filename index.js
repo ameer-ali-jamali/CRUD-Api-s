858a16efb2092a01c7a3eb97816ba18e1e61105f
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-28 02:52:35 +0500

    exporting routes to index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+const config = require('./config/config')
+
+const routes = require('./routes/web')
--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+const pubicPath = path.join(__dirname, 'public')
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(`${pubicPath}index.html`)
 })
+app.get('/about', (req, res) => {
+    res.sendFile(`${pubicPath}index.html`)
+})
+app.get('/contactUs', (req, res) => {
+    res.sendFile(`${pubicPath}index.html`)
+})
+module.exports = app
