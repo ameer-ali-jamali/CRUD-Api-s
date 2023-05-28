97face2c7ef121bd14a88999fa0df592ffe4ba35
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-28 18:38:55 +0500

    db connection

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const env = require('.env');
+require('dotenv').config();
 
-
+const app = express();
 const id = process.env.USER_ID;
 const pass = process.env.USER_KEY;
-
 const DB = `mongodb+srv://${id}:${pass}@cluster0.e2pewzd.mongodb.net/?retryWrites=true&w=majority`;
-const app = express(); ~
+mongoose.connect(DB).then((res) => {
+    if (res) {
+        console.log('db connected')
+    }
+}).catch((err) => console.log('DB Not Connected'))
 
-
-    app.get('/', (req, res) => {
-        res.send('Successful response.');
-    });
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.send('Successful response.');
+});
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+
+
+
+
+
+
+
+
+
+
+
