90aa3405e8541d9d1da02944c998920779437338
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-06-02 19:31:40 +0500

    creating table schema

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
-
 const app = express();
-const id = process.env.USER_ID;
-const pass = process.env.USER_KEY;
-const DB = `mongodb+srv://${id}:${pass}@cluster0.e2pewzd.mongodb.net/?retryWrites=true&w=majority`;
+const DB = ("mongodb://localhost:27017/std");
+
 mongoose.connect(DB).then((res) => {
     if (res) {
         console.log('db connected')
     }
 }).catch((err) => console.log('DB Not Connected'))
+const tableSchema = new mongoose.Schema({
+    id: Number,
+    first_name: String,
+    last_name: String,
+    email: String,
+    gender: String,
+    ip_address: String
+})
+
+
+
 
 app.get('/', (req, res) => {
-    res.send('Successful response.');
+    res.send(DB);
 });
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
-
-
-
-
-
-
-
-
-
-
-
+app.listen(5000, () => console.log('Example app is listening on port 4000.'));
