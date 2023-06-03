8497abab3148ad693f883e684b0e85a53d1e45a9
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-06-04 01:34:57 +0500

    get alluser api done

--- index.js
+++ index.js
@@ -9,30 +9,73 @@ mongoose.connect(DB).then((res) => {
     }
 }).catch((err) => console.log('DB Not Connected'))
 const tableSchema = new mongoose.Schema({
+    id: Number,
     first_name: String,
     last_name: String,
     email: String,
-    gender: String
+    gender: String,
 })
 
 const User = new mongoose.model("User", tableSchema)
 
-const insertData = async () => {
+// const insertData = async () => {
+//     try {
+//         const putData = new User({
+//             first_name: "Amjad",
+//             last_name: "Ali",
+//             email: "amjad@gmail.com",
+//             gender: "male"
+//         })
+//         const result = await putData.save();
+//         console.log(result)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// insertData();
+app.use(express.json());
+
+app.get('/user', async (req, res) => {
     try {
-        const putData = new User({
-            first_name: "Amjad",
-            last_name: "Ali",
-            email: "amjad@gmail.com",
-            gender: "male"
-        })
-        const result = await putData.save();
-        console.log(result)
+        const find = await User.find();
+        res.send(find)
     } catch (error) {
-        console.log(error)
+        res.send(error);
     }
-}
-insertData();
-app.get('/insert', (req, res) => {
-    res.send("data inserted");
 });
+app.put('/user/:id', async (req, res) => {
+    try {
+        const find = await User.findByIdAndUpdate(req.params.id);
+        res.send(find)
+    } catch (error) {
+        res.send(error);
+    }
+});
+// app.post('/user', async (req, res) => {
+//     try {
+//         const putData = new User(req.body);
+//         const result = await putData.save();
+//         res.send(result)
+//     } catch (error) {
+//         res.send(error);
+//     }
+// });
+app.put('/user/:id', async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const result = await User.findByIdAndUpdate(userId, req.body)
+        res.send(result)
+    } catch (error) {
+        res.send(error);
+    }
+});
+// app.delete('/user/:id', async (req, res) => {
+//     try {
+//         const userId = new User(req.params.id);
+//         const result = await putData.findByIdAndDelete(userId);
+//         res.send(result)
+//     } catch (error) {
+//         res.send(error);
+//     }
+// });
 app.listen(3000, () => console.log('Example app is listening on port 4000.'));
