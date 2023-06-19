2be58cb27c7cdbbfa62eb7993c828a685962dad1
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-06-19 16:42:45 +0500

    connecting altasdb

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const DB = ("mongodb://localhost:27017/std");
-mongoose.connect(DB).then((res) => {
-    if (res) {
-        console.log('db connected')
+const { MongoClient } = require('mongodb');
+const uri = process.env.ATLAS_URI;
+
+async function connect() {
+    try {
+        const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Connected to the Atlas database.');
+        client.close();
+        console.log('Connection closed.');
+    } catch (err) {
+        console.error('Error connecting to the Atlas database:', err);
     }
-}).catch((err) => console.log('DB Not Connected'))
+}
+connect();
+
+// const DB = ("mongodb://localhost:27017/std");
+// mongoose.connect(DB).then((res) => {
+//     if (res) {
+//         console.log('db connected')
+//     }
+// }).catch((err) => console.log('DB Not Connected'))
 const tableSchema = new mongoose.Schema({
     first_name: String,
     last_name: String,
